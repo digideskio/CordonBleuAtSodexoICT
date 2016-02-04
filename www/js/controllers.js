bd43b45012e9a5e32bdbe85b5b804d7a1a2761af
Sami Suo-Heikki <suoheikki.sami@gmail.com>
2016-02-04 20:43:10 +0200

    Index page variations if cordon server or not

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,12 +2,20 @@ angular.module('starter.controllers', [])
 
   .controller('IndexCtrl', function($scope, Menu, CordonComing) {
     $scope.cordon = false;
+    $scope.nocordon = false;
     Menu.then(function(menu){
-      $scope.cordon = CordonComing.get(menu);
-      console.log($scope.cordon);
+      var cordon = CordonComing.get(menu);
+      if (cordon) {
+        var cordonDate = new Date(cordon);
+        var day = cordonDate.getDate();
+        var month = cordonDate.getMonth()+1;
+        var year = cordonDate.getFullYear();
+        $scope.cordonDate = day+'.'+month+'.'+year;
+        $scope.cordon = true;
+      } else {
+        $scope.nocordon = true;
+      }
     });
-    $scope.success = 'false';
-
   })
   .controller('MenuCtrl', function($scope, Menu) {
     $scope.success = 'false';
